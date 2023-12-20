57e981191b9297ca50642124f9364a20f2abc62d
Dhanush <dhanushkrish54@gmail.com>
2023-12-20 16:57:18 +0530

    Update arrays.ts | 4:57PM

--- arrays.ts
+++ arrays.ts
@@ -59,4 +59,35 @@ favoriteCoordinates = [17, 45, 'N', 142, 30, 'E'];
 
 
 // *************** Rest Parameters ***************** //
+function addPower(p: number, ...numsToAdd: number[]): number{
+  let answer = 0; 
+  for(let i = 0; i < numsToAdd.length; i++){
+    answer += numsToAdd[i] ** p;
+  }
+  return answer;
+}
+addPower('a string', 4, 5, 6);
+
+
+// *************** Spread Syntax ***************** //
+function performDanceMove(moveName:string, moveReps:number, hasFlair:boolean):void{
+  console.log(`I do the ${moveName} ${moveReps} times !`);
+  if(hasFlair){
+    console.log('I do it with flair!');
+  }
+}
+
+let danceMoves: [string, number, boolean][] = [
+  ['chicken beak', 4, false],
+  ['wing flap', 4, false],
+  ['tail feather shake', 4, false],
+  ['clap', 4, false],
+  ['chicken beak', 4, true],
+  ['wing flap', 4, true],
+  ['tail feather shake', 4, true],
+  ['clap', 4, true],
+];
+
+danceMoves.forEach(move => performDanceMove(...move));
+
 
